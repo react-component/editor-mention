b2206e77e798bae69bd419f0db3460e9815f85ca
RaoHai <surgesoft@gmail.com>
2016-07-21 17:38:47 +0800

    Updates

--- examples/common.js
+++ examples/common.js
@@ -40566,8 +40566,6 @@
 	
 	var _react2 = _interopRequireDefault(_react);
 	
-	var _actions = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./actions\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
-	
 	var _draftJs = __webpack_require__(179);
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
